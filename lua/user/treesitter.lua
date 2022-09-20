d0608791d8c482616f7e85d31aaff1d07d623a97
iusztin <iusztin@gmail.com>
2022-09-20 13:37:53 +0900

    Configure treesitter languages individually

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = { "go", "gomod", "ruby", "python", "html", "json", "javascript", "typescript", "yaml", "lua", "bash", "css", "scss", "tsx", "markdown", "kotlin" }, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
