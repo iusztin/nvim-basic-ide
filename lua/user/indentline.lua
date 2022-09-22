0162c1ee393371a372ebb05b9851a623b870338a
iusztin <iusztin@gmail.com>
2022-09-22 10:49:21 +0900

    Reconfigure indent blankline

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,12 +1,11 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
-if not status_ok then
-  return
-end
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
 
-indent_blankline.setup {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
+require("indent_blankline").setup {
+  char = "",
+  show_trailing_blankline_indent = true,
+  show_end_of_line = false,
+  show_first_indent_level = false,
   use_treesitter = true,
   show_current_context = true,
   buftype_exclude = { "terminal", "nofile" },
