988f72648cd5dbd926fcdb5a9bd4f5fad2993e0c
iusztin <iusztin@gmail.com>
2022-09-22 11:01:46 +0900

    Avoid hiding require errors in some cases

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,9 +1,4 @@
-local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-	return
-end
-
-configs.setup({
+require("nvim-treesitter.configs").setup({
 	ensure_installed = { "go", "gomod", "ruby", "python", "html", "json", "javascript", "typescript", "yaml", "lua", "bash", "css", "scss", "tsx", "markdown", "kotlin" }, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
