69f30efc6e95ccdf75545124d9dde19f7bfd809b
iusztin <iusztin@gmail.com>
2022-10-30 18:42:29 +0900

    feat: add mappings for luasnip

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -124,3 +124,21 @@ cmp.setup({
 		ghost_text = true,
 	},
 })
+
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+	if luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+	if luasnip.jumpable(-1) then
+		luasnip.jump(-1)
+	end
+end, { silent = true })
+
+vim.keymap.set("i", "<c-l>", function()
+	if luasnip.choice_active() then
+		luasnip.change_choice(1)
+	end
+end)
