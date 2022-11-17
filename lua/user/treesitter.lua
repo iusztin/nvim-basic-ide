a98f75d34f1a831f9664d99ac18bedba5542ad02
iusztin <iusztin@gmail.com>
2022-11-17 11:00:02 +0900

    chore: replace tabs with spaces

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,11 +1,12 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "gomod", "ruby", "python", "html", "json", "javascript", "typescript", "yaml", "lua", "bash", "css", "scss", "tsx", "markdown", "kotlin" }, -- one of "all" or a list of languages
-	ignore_install = { "" }, -- List of parsers to ignore installing
-	highlight = {
-		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
-	},
-	autopairs = {
-		enable = true,
-	},
+  ensure_installed = { "go", "gomod", "ruby", "python", "html", "json", "javascript", "typescript", "yaml", "lua", "bash",
+    "css", "scss", "tsx", "markdown", "kotlin" }, -- one of "all" or a list of languages
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    enable = true, -- false will disable the whole extension
+    disable = { "css" }, -- list of language that will be disabled
+  },
+  autopairs = {
+    enable = true,
+  },
 })
