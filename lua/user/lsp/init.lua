b85bb7903ff0eb2798cb73b8e7168416a223e317
iusztin <iusztin@gmail.com>
2023-08-20 11:34:35 +0900

    chore: avoid redundant require in lsp init

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,6 +1,5 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("lspconfig")
-require "user.lsp.lspconfig"
+require("user.lsp.lspconfig")
 require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.null-ls")
