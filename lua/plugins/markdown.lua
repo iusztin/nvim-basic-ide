57d7e63683ad6e68b6830821f3301598a65ebff2
iusztin <iusztin@gmail.com>
2025-08-31 06:57:39 +0900

    feat!: use nabla to render latex in markdown

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,7 +1,20 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
+    -- LaTeX
+    'jbyuki/nabla.nvim',
+  },
+  opts = {
+    latex = { enabled = false },
+    on = {
+      render = function()
+        require('nabla').enable_virt()
+      end,
+      clear = function()
+        require('nabla').disable_virt()
+      end,
+    },
+  },
 }
