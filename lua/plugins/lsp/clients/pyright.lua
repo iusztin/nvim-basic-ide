886341420ce4b719b1f14e7056767bc3a3472d0c
iusztin <iusztin@gmail.com>
2025-09-18 18:10:43 +0900

    feat!: support virtualenv for poetry v2

--- lua/plugins/lsp/clients/pyright.lua
+++ lua/plugins/lsp/clients/pyright.lua
@@ -1,7 +1,7 @@
 local function get_python_path()
   local python_home = vim.trim(vim.fn.system('poetry env info -p'))
   local python_path = 'python'
-  if string.find(python_home, ".venv", 1, true) then
+  if string.find(python_home, "virtualenvs", 1, true) then
     python_path = string.format("%s/bin/python", python_home)
   end
 
