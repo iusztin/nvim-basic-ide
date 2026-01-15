874582caad7f11a747d6561d0022d0b4dd1d4ee6
iusztin <iusztin@gmail.com>
2026-01-15 15:18:17 +0900

    chore!: drop nabla due to conceal issues in unrelated context

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,18 +3,8 @@ return {
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
-    -- LaTeX
-    'jbyuki/nabla.nvim',
   },
   opts = {
     latex = { enabled = false },
-    on = {
-      render = function()
-        require('nabla').enable_virt()
-      end,
-      clear = function()
-        require('nabla').disable_virt()
-      end,
-    },
   },
 }
